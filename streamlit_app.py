e706f99d5dc1157d67e5db3e9afc6a63f967ae84
Vikaas Subhashini Rajkumar <110914754+Vikaas161@users.noreply.github.com>
2024-04-29 12:57:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,10 @@ ingredients_list= st.multiselect('Choose upto 5 ingredients: ',my_dataframe,max_
 
 if ingredients_list:
 
-    ingredients_string = ' '
+    ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ''
 
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
